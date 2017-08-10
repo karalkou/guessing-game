b1c465ea470e809abb002345882734ba493a58b5
Yury Karalkou <yury.karalkou@gmail.com>
2017-08-10 18:01:49 +0300

    Start to do task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,44 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.rangeArr = [];
+        this.maxIndex = null;
+        this.minIndex = null;
+        this.guessingNum = null;
+    }
 
     setRange(min, max) {
-
+        /* some logic */
     }
 
     guess() {
-
+        /* some logic */
+        return this.guessingNum;
     }
 
     lower() {
-
+        this.maxIndex = this.guessingNum;
     }
 
     greater() {
+        this.minIndex = this.guessingNum;
+    }
 
+    someLogic(arr, data){
+        let upperBound = arr.length-1;
+        let lowerBound = 0;
+        while (lowerBound <= upperBound) {
+            let mid = Math.floor((upperBound + lowerBound) / 2);
+            if (arr[mid] < data) {
+                lowerBound = mid + 1;
+            }
+            else if (arr[mid] > data) {
+                upperBound = mid - 1;
+            }
+            else {
+                return mid;
+            }
+        }
+        return -1;
     }
 }
 
