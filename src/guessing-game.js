21219dcfb529a53bc85f8c852683ac342f230700
Yury Karalkou <yury.karalkou@gmail.com>
2017-09-16 15:01:31 +0300

    Remove logs and garbage

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,34 +1,28 @@
 class GuessingGame {
     constructor() {
-        this.rangeArr = [];
         this.maxIndex = null;
         this.minIndex = null;
         this.guessingNum = null;
     }
 
     setRange(min, max) {
-        this.rangeArr.length = 0;
         if(min < max){
             this.maxIndex = max;
             this.minIndex = min;
-            console.log("----setRange: ", this.maxIndex, this.minIndex, this.guessingNum);
         }
     }
 
     guess() {
         this.guessingNum = Math.round((this.maxIndex + this.minIndex) / 2);
-        console.log("----guess: ", this.maxIndex, this.minIndex, this.guessingNum);
         return this.guessingNum;
     }
 
     lower() {
         this.maxIndex = this.guessingNum;
-        console.log("----lower: ",this.maxIndex, this.minIndex, this.guessingNum);
     }
 
     greater() {
         this.minIndex = this.guessingNum;
-        console.log("----greater: ", this.maxIndex, this.minIndex, this.guessingNum);
     }
 }
 
